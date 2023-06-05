6a93a1e5acad37262152e95fc42b3077fdaf58af
cwwyoung <cwwyoung@gmail.com>
2023-06-05 19:01:52 -0400

    fixed up gallery modal thing

--- script.js
+++ script.js
@@ -1,21 +1,49 @@
-const modal = document.querySelector('.insideCard');
-const gallery = document.querySelector('.gallery')
-const btnCloseModal = document.querySelector('.fa-x');
-const openModal = document.querySelector('.openModal');
-const desc = document.querySelector('p.desc');
+//gallery popups
 
-openModal.addEventListener('click', function (event) {
-    modal.classList.add('transform');
-    desc.classList.remove('hidden')
-    btnCloseModal.classList.remove('hidden')
-    const img = event.target.closest('a');
-    img.style.textAlign = 'center';
+const modals = document.querySelectorAll('.insideCard');
+const galleries = document.querySelectorAll('.gallery');
+const btnCloseModals = document.querySelectorAll('.fa-x');
+const openModals = document.querySelectorAll('.openModal');
+const descs = document.querySelectorAll('p.desc');
 
-})
+openModals.forEach(function (openModal, index) {
+    openModal.addEventListener('click', function (event) {
+        if (!modals[index].classList.contains('transform')) {
+            closeAllModals();
+        }
+        modals[index].classList.add('transform');
+        descs[index].classList.remove('hidden');
+        btnCloseModals[index].classList.remove('hidden');
+        const img = event.target.closest('a');
+        img.style.textAlign = 'center';
+    });
+});
 
-btnCloseModal.addEventListener('click', function (e) {
-    modal.classList.remove('transform');
-    btnCloseModal.classList.add('hidden');
-    desc.classList.add('hidden');
-    console.log(desc)
-})
+btnCloseModals.forEach(function (btnCloseModal, index) {
+    btnCloseModal.addEventListener('click', function (e) {
+        closeModal(index);
+    });
+});
+
+document.addEventListener('keydown', function (event) {
+    if (event.key === "Escape") {
+        closeAllModals();
+    }
+});
+
+function closeModal(index) {
+    modals[index].classList.remove('transform');
+    btnCloseModals[index].classList.add('hidden');
+    descs[index].classList.add('hidden');
+}
+
+function closeAllModals() {
+    modals.forEach(function (modal, index) {
+        closeModal(index);
+    });
+};
+
+
+// contact error handling
+
+
